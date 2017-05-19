7ff722024d3014393dd7a5d884311d85dd26c949
Helena Richie <helenarichie@gmail.com>
2017-05-19 13:00:53 -0400

    isr_removal opens each individual and saves it rather than looping through an array

---
+++ Calibration/InstrumentSignature_Removal/isr_removal.py
@@ -0,0 +1,37 @@
+from astropy.io import fits
+import numpy as np
+
+def isr_removal(target, mbias, mdark, mflat, dark_exptime):
+    """Creates reduced array of target images.
+
+    Loops through images in dirtarget
+
+    Parameters
+    ----------
+    mbias : numpy array
+        2D array containing master bias image.
+    mdark : numpy array
+        2D array containing master dark image.
+    mflat : numpy array
+        2D array containing master flat image.
+    dark_exptime : float
+        Module variable that is the exposure time of the dark images.
+
+    Returns
+    -------
+    isr_science_images : numpy array
+        3D array containing instrument signature removed array of
+        target images.
+    """
+
+    for image in dirtarget:
+        hdulist = fits.open(image)
+        if hdulist[0].header['IMAGETYP'] == 'Light Frame':
+            image -= dark_exptime*mdark
+            image -= mbias
+            image /= mflat
+            hdulist.writeto(dirstar + '/isr_science_image/' + image)
+
+    
+             
+            
