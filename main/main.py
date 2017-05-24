20015c582acf949fa1cbc2b746f640e53373560e
Helena Richie <helenarichie@gmail.com>
2017-05-24 15:18:59 -0400

    adds saturation in isr function, removed calibration function

--- main/main.py
+++ main/main.py
@@ -1,28 +1,43 @@
 import sys
-sys.path.insert(0, 'directory where code is stored')
-import get_calibimages 
+sys.path.insert(0, '/Users/helenarichie/GitHub/STEPUP_image_analysis/ISR')
+import get_calibimages
 import create_mcalib
-import isr_removal
-import calibration
+import instrument_signature_removal
 
-date = input('Enter the date of observation (MM/DD/YYYY): ')
-target = input('Enter the name of the target: ')
-dirtarget = '/home/depot/STEPUP/raw/' + date
-dirdark = '/home/depot/STEPUP/raw/calibration/Dark/default'
+dirtarget = dirdark = '/Users/helenarichie/HAT-P-3_Test_Images'
 
 def main(dirtarget, dirdark):
-    
+    """Creates ISR FITS files by executing a preliminary calibration sequence.
+
+    Imports all ISR modules (get_calibimage, instrument_signature_removal, and calibration)
+    which get all calibration images, create master calibration images, and
+    subtract them from the raw science images. It then adds the expected
+    saturation to the header and saves the ISR science images to the specified
+    directory.
+
+    Parameters
+    ----------
+    dirtarget : str
+        Directory containing all bias, flat, and raw science images.
+    dirdark : str
+        Directory containing all dark images.
+
+    Returns
+    -------
+    None
+    """
+
     (biases, darks, flats, dark_exptime) = get_calibimages.get_calibimages(dirtarget, dirdark)
-    
+
     mbias = create_mcalib.create_mbias(biases)
     mdark = create_mcalib.create_mdark(darks, mbias)
     mflat = create_mcalib.create_mflat(flats, mbias, mdark)
 
-    isr_scimage = isr_removal.isr_removal(dirtarget, mbias, mdark, mflat, dark_exptime)
+    instrument_signature_removal.instrument_signature_removal(dirtarget, mbias, mflat, mdark, dark_exptime)
 
-    isr_scimage = calibration.calibration(mbias, mdark, mflat, dark_exptime, isr_scimage)
-       
-main(dirtarget, dirdark)
+    
+
     
     
+
     
