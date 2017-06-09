99cdeeaa1ccc8834eb0b1d841b681e803621d580
Helena Richie <helenarichie@gmail.com>
2017-06-09 13:49:47 -0400

    Incorporating options for different filters

--- ISR_main/ISR/instrument_signature_removal.py
+++ ISR_main/ISR/instrument_signature_removal.py
@@ -35,45 +35,132 @@ def instrument_signature_removal(dirtarget, target, mbias, mdark, mflat, dark_ex
         3D array containing all science images with instrument signatures
         removed.
     """
-
-    saturation = 65535
-    saturation -= np.median(mbias)
-    saturation -= np.median((mdark*exptime)/dark_exptime)
-    saturation /= np.average(mflat)
-    saturation *= 0.97
-
-    files = glob.glob(os.path.join(dirtarget, '*.fit'))
-    scimages = []
-    prihdr = None
-
-    for file in files:
-        hdulist = fits.open(file)
-        if hdulist[0].header['IMAGETYP'] == 'Light Frame':
-            hdulist[0].header['SATLEVEL'] = (saturation)
-            exptime = hdulist[0].header['EXPTIME']
-            prihdr = hdulist[0].header
-            image = fits.getdata(file)
-            scimages.append(image)
-        hdulist.close()
-        
-    scimages = np.array(scimages, dtype=float)
-    isr_scimages = []
-    n = 0
-
-    for file in scimages:
-            file -= mbias
-            file -= mdark
-            file /= mflat
-            isr_scimages.append(file)
-            
-    isr_scimages = np.array(isr_scimages, dtype = float)
+    r_saturation = None
+    b_saturation = None
+    v_saturation = None
+    r_scimages = []
+    b_scimages = []
+    v_scimages = []
+    rim_prihdr = None
+    bim_prihdr = None
+    vim_prihdr = None
+    r_isr_scimages = None
+    b_isr_scimages = None
+    v_isr_scimages = None
 
     os.mkdir(dirtarget + '/ISR_Images')
+    
+    files = glob.glob(os.path.join(dirtarget, '*.fit'))
+    
+    if r_filter:
+        r_saturation = 65535
+        r_saturation -= np.median(mbias)
+        r_saturation -= np.median((mdark*exptime)/dark_exptime)
+        r_saturation /= np.average(r_mflat)
+        r_saturation *= 0.97
 
-    for i in isr_scimages:
-        n+=1
-        hdu = fits.PrimaryHDU(i,header=prihdr)
-        hdulist = fits.HDUList([hdu])
-        hdulist.writeto(dirtarget + '/ISR_Images/' + target + '_{}.fits'.format(n),
-                        overwrite=True)
-    return isr_scimages
+        for file in files:
+            hdulist = fits.open(file)
+            if hdulist[0].header['IMAGETYP'] == 'Light Frame':
+                if hdulist[0].header['FILTER'] == 'r':
+                    hdulist[0].header['SATLEVEL'] = r_saturation
+                    exptime = hdulist[0].header['EXPTIME']
+                    rim_prihdr = hdulist[0].header
+                    image = fits.getdata(file)
+                    r_scimages.append(image)
+            hdulist.close()
+
+        r_scimages = np.array(r_scimages, dtype=float)
+        r_isr_scimages = []
+        n = 0
+
+        for file in scimages:
+                file -= mbias
+                file -= mdark
+                file /= r_mflat
+                r_isr_scimages.append(file)
+                
+        r_isr_scimages = np.array(r_isr_scimages, dtype = float)
+
+        for i in r_isr_scimages:
+            n+=1
+            hdu = fits.PrimaryHDU(i,header=rim_prihdr)
+            hdulist = fits.HDUList([hdu])
+            hdulist.writeto(dirtarget + '/ISR_Images/' + target + '-R_{}.fits'.format(n),
+                            overwrite=True)
+
+    if b_filter:
+        b_saturation = 65535
+        b_saturation -= np.median(mbias)
+        b_saturation -= np.median((mdark*exptime)/dark_exptime)
+        b_saturation /= np.average(b_mflat)
+        b_saturation *= 0.97
+
+        for file in files:
+            hdulist = fits.open(file)
+            if hdulist[0].header['IMAGETYP'] == 'Light Frame':
+                if hdulist[0].header['FILTER'] == 'b':
+                    hdulist[0].header['SATLEVEL'] = b_saturation
+                    exptime = hdulist[0].header['EXPTIME']
+                    bim_prihdr = hdulist[0].header
+                    image = fits.getdata(file)
+                    b_scimages.append(image)
+            hdulist.close()
+
+        b_scimages = np.array(b_scimages, dtype=float)
+        b_isr_scimages = []
+        n = 0
+
+        for file in scimages:
+                file -= mbias
+                file -= mdark
+                file /= b_mflat
+                b_isr_scimages.append(file)
+                
+        b_isr_scimages = np.array(b_isr_scimages, dtype = float)
+
+        for i in b_isr_scimages:
+            n+=1
+            hdu = fits.PrimaryHDU(i,header=bim_prihdr)
+            hdulist = fits.HDUList([hdu])
+            hdulist.writeto(dirtarget + '/ISR_Images/' + target + '-B_{}.fits'.format(n),
+                            overwrite=True)
+
+    if v_filter:
+        v_saturation = 65535
+        v_saturation -= np.median(mbias)
+        v_saturation -= np.median((mdark*exptime)/dark_exptime)
+        v_saturation /= np.average(b_mflat)
+        v_saturation *= 0.97
+
+        for file in files:
+            hdulist = fits.open(file)
+            if hdulist[0].header['IMAGETYP'] == 'Light Frame':
+                if hdulist[0].header['FILTER'] == 'v':
+                    hdulist[0].header['SATLEVEL'] = v_saturation
+                    exptime = hdulist[0].header['EXPTIME']
+                    prihdr = hdulist[0].header
+                    image = fits.getdata(file)
+                    v_scimages.append(image)
+            hdulist.close()
+        
+        v_scimages = np.array(v_scimages, dtype=float)
+        v_isr_scimages = []
+        n = 0
+
+        for file in scimages:
+                file -= mbias
+                file -= mdark
+                file /= v_mflat
+                v_isr_scimages.append(file)
+                
+        v_isr_scimages = np.array(v_isr_scimages, dtype = float)
+
+        for i in v_isr_scimages:
+            n+=1
+            hdu = fits.PrimaryHDU(i,header=vim_prihdr)
+            hdulist = fits.HDUList([hdu])
+            hdulist.writeto(dirtarget + '/ISR_Images/' + target + '-V_{}.fits'.format(n),
+                            overwrite=True)
+
+    return r_isr_scimages, b_isr_scimages, v_isr_scimages
