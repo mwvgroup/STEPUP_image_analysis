b0f362b3841df367324b594ca68a9fa3df8c3821
Helena Richie <helenarichie@gmail.com>
2017-06-16 11:22:13 -0400

    Removed user input to obtain filters.

--- main.py
+++ main.py
@@ -8,14 +8,5 @@ date = input('Input date of observation (MM/DD/YYYY): ')
 target = input('Input target name: ')
 dirtarget = '/home/depot/STEPUP/raw' + date
 dirdark = '/home/depot/STEPUP/Calibration'
-filters = []
-
-im_filter = input('Input filter name (if finished type "Done"): ')
-filters.append(im_filter)
-while im_filter != 'Done':
-    im_filter = input('Input filter name (if finished type "Done"): ')
-    filters.append(im_filter)
-
-filters.remove('Done')
 
     
