ab274c4593d2bef09b9071a70eb9c09dbbd0f769
Helena Richie <helenarichie@gmail.com>
2017-08-16 13:04:25 -0400

    Restructure so that add_WCS_info is separated into its own file called perform_astrometry.

--- main.py
+++ main.py
@@ -2,32 +2,43 @@ import sys
 sys.path.insert(0, '/Users/helenarichie/GitHub/STEPUP_image_analysis/ISR')
 import ISR
 sys.path.insert(0, '/Users/helenarichie/GitHub/STEPUP_image_analysis/Calibration')
-import add_WCS_info
+import perform_astrometry
+import perform_photometry
 
-date = input('Input date of observation (YYYY-MM-DD): ')
-target = input('Input target name: ')
-dirtarget = '/Users/helenarichie/tests2'
-dirdark = dirtarget
-ra = input('Input RA of target (HH:MM:SS): ')
-dec = input('Input Dec of target (+/-DD:MM:SS): ')
-coords = (ra, dec)
-ra_c = input('Input RA of comparison star (HH:MM:SS): ')
-dec_c = input('Inpur Dec of comparison star (+/-DD:MM:SS): ')
-comparison_coords = (ra_c, dec_c)
 
-filters = ISR.ISR_main(dirtarget, dirdark, target)
+def main(verbose=False):
+    date = input('Input date of observation (YYYY-MM-DD): ')
+    target = input('Input target name: ')
+    dirtarget = '/Users/helenarichie/tests2'
+    dirdark = dirtarget
+    ra = input('Input RA of target (HH:MM:SS): ')
+    dec = input('Input Dec of target (+/-DD:MM:SS): ')
+    coords = (ra, dec)
+    ra_c = input('Input RA of comparison star (HH:MM:SS): ')
+    dec_c = input('Inpur Dec of comparison star (+/-DD:MM:SS): ')
+    comp_coords = (ra_c, dec_c)
 
-answer = print('\nInstrument signature removal completed.',
-               'Continue to photometry? (Y/N): ')
-if answer == 'Y':
-    print('\nPhotometry in progress...')
-else:
-    return None
+    filters = ISR.ISR_main(dirtarget, dirdark, target)
 
-dirtarget += '/ISR_Images'
+    answer = print('\nInstrument signature removal completed.',
+                   'Continue to astrometry (Y/N): ')
+    answer = answer.upper()
+    if answer == 'Y':
+        print('\nAstrometry in progress...')
+    else:
+        return None
 
-add_WCS_info.add_WCS_info(target, dirtarget, filters, verbose=True)
+    dirtarget += '/ISR_Images'
 
-perform_photometry.perform_photometry(target, dirtarget, filters, date, coords,
-                                      comparison_coords, comparison_mag,
-                                      verbose=False)
+    perform_astrometry.perform_astrometry(target, dirtarget, filters, verbose=False)
+
+    answer = print('\nAstrometry completed.',
+                   'Continue to photometry? (Y/N): ')
+    answer = answer.upper()
+    if answer == 'Y':
+        print('\nAstrometry in progress...')
+    else:
+        return None
+
+    perform_photometry.perform_photometry(target, dirtarget, filters, date, coords,
+                                          comp_coords, comp_mag, verbose=False)
