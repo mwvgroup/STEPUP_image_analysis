2791784c117eef1d5a4d56e4eab7f9242098a7ed
Helena Richie <helenarichie@gmail.com>
2018-12-06 12:11:57 -0500

    Try different aperture sizes.

--- Calibration/perform_photometry.py
+++ Calibration/perform_photometry.py
@@ -544,9 +544,9 @@ def get_counts(dirtarget, rightascension, declination, fil):
                 continue
 
             # Define aperture and annulus radii.
-            radius = 6.8892 * u.arcsec
-            r_in = 12.17092 * u.arcsec
-            r_out = 16.0748 * u.arcsec
+            radius = 7 * u.arcsec
+            r_in = 13 * u.arcsec
+            r_out = 16 * u.arcsec
 
             # Create SkyCircularAperture and SkyCircularAnnulus objects
             # centered at the position of the star whose counts are being
